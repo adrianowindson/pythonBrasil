f1482fec28a434d62eac22e65f0bb0b27485643d
Adriano Lima <adrianowindsonbjj@gmail.com>
2020-03-20 21:27:17 -0300

    ;)

---
+++ 01_EstruturaSequencial/03_somaNum.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# 3- Faça um Programa que peça dois números e imprima a soma.
+
+a = int(input("Entre com um número: "))
+b = int(input("Entre com número a ser somado: "))
+c = a + b
+print (" A soma dos números %s + %s é igual a: %s " %(a,b,c))

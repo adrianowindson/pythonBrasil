80f565df2c1d9b043ac2e02b030031723c3b6323
Adriano Lima <adrianowindsonbjj@gmail.com>
2020-03-21 00:45:16 -0300

    ;)

---
+++ 01_EstruturaSequencial/06_raio.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+pi = 3.14
+r = float(input("Entre com o valor do raio do círculo: "))
+area = (r ** 2) * pi
+print("O tamanho da área é: %s" %(area))

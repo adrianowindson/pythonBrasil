f1f5847f7bca54d6f278b3bef61c091b5e9fdb26
Adriano Lima <adrianowindsonbjj@gmail.com>
2020-03-20 21:29:18 -0300

    ;)

---
+++ 01_EstruturaSequencial/05_metrosParaCm.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 5 - Faça um Programa que converta metros para centímetros.
+
+m = float(input("Entre com o valor em M a ser convertido para Cm: "))
+cm = m * 100
+print("O valor convertido em cm é: %s" %(cm))
